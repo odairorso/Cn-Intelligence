d079a4e015333a36e260cde01ddc28e7a1e95e38
Odair Orso <odairorso@users.noreply.github.com>
2026-05-12 08:34:31 -0400

    fix: wrap App with AppDataProvider to resolve context error

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { AppDataProvider } from './hooks/useAppData.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppDataProvider>
+      <App />
+    </AppDataProvider>
   </StrictMode>,
 );
